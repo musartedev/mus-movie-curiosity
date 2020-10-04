03c93643a627bc3b0679900ecfca272607fab0c8
Mariangélica Useche <mariangelica.useche@gmail.com>
2020-10-04 00:24:24 -0500

    Add MovieList Feed

--- src/frontend/components/Layout/index.jsx
+++ src/frontend/components/Layout/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import SearchBox from './SearchBox';
+import MovieList from '../common/MovieList';
 import Footer from './Footer';
+
+import movieList from '../../constants/MovieData';
 
 import '../../styles/components/Layout/index.styl';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="Layout">
       <Header />
       <SearchBox />
-      <div className="container">{children}</div>
+      <div>
+        <MovieList title="Now Playing" movieList={movieList} />
+        <MovieList title="Trending" movieList={movieList} />
+      </div>
       <Footer />
     </div>
   );
